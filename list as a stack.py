0504fe629b10081baa3bc05199e52dee92fc78c6
SyabAhmad <syabblogger@gmail.com>
2023-03-06 14:09:30 +0500

    stack and queue

---
+++ list as a stack.py
@@ -0,0 +1,9 @@
+print("List as a stack data structure")
+
+
+stack = ["Apple", "Banana", "Grapes", "PineApple"]
+print(stack) #print stack
+stack.pop(1) # remove the value on 1 index which is 6
+print(stack) #print stack
+stack.append("Mango") # append Mango value in stack
+print(stack) #print stack
