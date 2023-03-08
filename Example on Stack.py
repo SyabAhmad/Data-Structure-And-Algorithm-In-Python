774ba92ad30acef8379bdaf33e1091ee4fb5266a
SyabAhmad <syabblogger@gmail.com>
2023-03-08 19:37:12 +0500

    Updates in ...

---
+++ Example on Stack.py
@@ -0,0 +1,21 @@
+# Create an empty stack
+stack = []
+
+# Push items onto the stack
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+# Pop items off the stack
+print(stack.pop())  # Output: 3
+print(stack.pop())  # Output: 2
+print(stack.pop())  # Output: 1
+
+
+
+                            # In this example, we create an empty list and use
+                            # the append() method toadd items onto the stack.
+                            # We then use the pop() method to remove items
+                            # from the stack in last-in, first-out (LIFO)
+                            # order. The output of the pop()method is the
+                            # last item that was added to the stack
